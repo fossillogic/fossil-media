4fd0a43ec4ac4b4124ccb5273e31a4f9b8eba61d
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-09-06 19:50:23 -0600

    this time

--- code/tests/cases/test_json.cpp
+++ code/tests/cases/test_json.cpp
@@ -85,77 +85,6 @@ FOSSIL_TEST_CASE(cpp_test_json_stringify_roundtrip) {
     ASSUME_ITS_TRUE(out == src || out == "{\"foo\":[1,true,null]}");
 }
 
-FOSSIL_TEST_CASE(cpp_test_json_clone_deep_copy) {
-    Json orig = Json::parse("{\"x\":42,\"y\":[1,2]}");
-    Json copy = orig.clone();
-    ASSUME_ITS_TRUE(orig.equals(copy));
-    // Mutate copy, orig should not change
-    Json arr = copy.get_path("y");
-    arr = Json::parse("[99]");
-    ASSUME_ITS_FALSE(orig.equals(copy));
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_equals_true_and_false) {
-    Json a = Json::parse("[1,2,3]");
-    Json b = Json::parse("[1,2,3]");
-    Json c = Json::parse("[1,2,4]");
-    ASSUME_ITS_TRUE(a.equals(b));
-    ASSUME_ITS_FALSE(a.equals(c));
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_is_null) {
-    Json j = Json::parse("null");
-    ASSUME_ITS_TRUE(j.is_null());
-    Json k = Json::parse("42");
-    ASSUME_ITS_FALSE(k.is_null());
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_is_array_and_object) {
-    Json arr = Json::parse("[1,2]");
-    Json obj = Json::parse("{\"a\":1}");
-    ASSUME_ITS_TRUE(arr.is_array());
-    ASSUME_ITS_FALSE(arr.is_object());
-    ASSUME_ITS_TRUE(obj.is_object());
-    ASSUME_ITS_FALSE(obj.is_array());
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_array_reserve_and_object_reserve) {
-    Json arr = Json::parse("[1]");
-    arr.array_reserve(10); // Should not throw
-    Json obj = Json::parse("{\"a\":1}");
-    obj.object_reserve(5); // Should not throw
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_new_int_and_get_int) {
-    Json j = Json::new_int(123456789);
-    ASSUME_ITS_EQUAL_I32(j.get_int(), 123456789);
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_validate_valid_and_invalid) {
-    ASSUME_ITS_TRUE(Json::validate("{\"a\":1}"));
-    ASSUME_ITS_FALSE(Json::validate("{a:1}")); // Invalid JSON
-}
-
-FOSSIL_TEST_CASE(cpp_test_json_get_path_simple_and_nested) {
-    Json j = Json::parse("{\"user\":{\"name\":\"Bob\"},\"items\":[{\"id\":1},{\"id\":2}]}");
-    Json name = j.get_path("user.name");
-    ASSUME_ITS_EQUAL_CSTR(name.stringify().c_str(), "\"Bob\"");
-    Json id2 = j.get_path("items[1].id");
-    ASSUME_ITS_EQUAL_I32(id2.get_int(), 2);
-}
-
-// Note: parse_file and write_file require filesystem access, so only pseudo-tested here.
-FOSSIL_TEST_CASE(cpp_test_json_parse_file_and_write_file) {
-    // Setup: Write a temp file, then parse it
-    const std::string fname = "test_tmp.json";
-    Json j = Json::parse("{\"foo\":123}");
-    j.write_file(fname, false);
-    Json k = Json::parse_file(fname);
-    ASSUME_ITS_TRUE(j.equals(k));
-    // Cleanup: remove file (if possible)
-    std::remove(fname.c_str());
-}
-
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -168,15 +97,6 @@ FOSSIL_TEST_GROUP(cpp_json_tests) {
     FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_parse_array);
     FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_parse_object);
     FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_stringify_roundtrip);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_clone_deep_copy);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_equals_true_and_false);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_is_null);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_is_array_and_object);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_array_reserve_and_object_reserve);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_new_int_and_get_int);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_validate_valid_and_invalid);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_get_path_simple_and_nested);
-    FOSSIL_TEST_ADD(cpp_json_fixture, cpp_test_json_parse_file_and_write_file);
 
     FOSSIL_TEST_REGISTER(cpp_json_fixture);
 } // end of tests
